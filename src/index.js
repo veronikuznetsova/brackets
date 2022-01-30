80fb3347d16505a77c9208b5047d7b4d09b0fdf3
Veronika Kuznetsova <veronichka.kuznetsova.1997@mail.ru>
2022-01-30 18:38:13 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let config = bracketsConfig.map ( (e) => e.join('') );
+    
+    for (let i = 0; i < config.length; i++) {
+      if (str.includes (config[i]) ) {
+        str = str.replace (config[i], '');
+        i = -1;
+      }
+    }
+
+    return (str) ? false : true;
 }
